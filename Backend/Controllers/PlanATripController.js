a0a7914b63b5cb4661bd35fc7edc2f5d36080a2e
AsadGohar <53337734+AsadGohar@users.noreply.github.com>
2021-04-22 00:06:03 +0500

    updated PlanATripController.js
    added comments

--- Backend/Controllers/PlanATripController.js
+++ Backend/Controllers/PlanATripController.js
@@ -6,9 +6,28 @@ const HttpError = require('../Models/HttpError');
 
 const getTripPlanEstimate = async (req,res,next)=>{
   const {destinations} =req.body
-  let nextDest=1,minHotel=0,maxHotel=0,routes=[],fares=[],transports=[],totalFare=0,minEstimate,maxEstimate,newFares=[],minTransport=0,maxTransport=0;
+  //nextDest iterator variable
+  //minHotel for minimum hotel rent
+  //maxHotel for maximumhotel rent
+  //transport[] for inserting transport objects who have the route
+  //routesDetails[] for insering subarrays of objects {transport id,fares,to,from} 
+  //minTransportFare for storing minimum transport expenditure
+  //maxTransportFare for storing maximum transport expenditure
+  //totalFare to calculate total fare (old logic)
+  //routes[] for inserting routes info (old logic)
+  //fares[] for inserting fare values of routes (old logic)
+  //maxEstimate for sending estimated values from the controller  (old logic)
+  //minEstimate for sending estimated values from the controller (old logic)
+
+  //old logic used to calculate estimates by adding the top transport fare from each transport array
+  //new logic used to calculate estimates by adding minimum and maximum transport fare from each transport array
+
+  let nextDest=1,minHotel=0,maxHotel=0,routes=[],fares=[],transports=[],totalFare=0,minEstimate,maxEstimate,routesDetails=[],minTransportFare=0,maxTransportFare=0;
+
+  //checking if any transport has routes between the destinations
   for (let index = 0; index < destinations.length-1; index++) {
 
+    //finding in subdocument array of routes , returns an array of transports where conditions match in subdocument array of routes
     let transport = await TransportModel.find
     ( 
       {"routes": 
@@ -20,104 +39,88 @@ const getTripPlanEstimate = async (req,res,next)=>{
         }
       }
     );
+      //if no such transport available with the route available
     if (transport.length===0){
       let to = await TripPlannerDestinationModel.findById(destinations[index])
       let from = await TripPlannerDestinationModel.findById(destinations[nextDest])
       const error = new HttpError(`could not find route between ${to.name} to ${from.name}`,404);
       return next(error);
     }
+    //if transport available
     else {
-      // console.log(transport)
       transports.push(transport)
 
       let from = destinations[index]
       let to = destinations[nextDest]
-      // let nroutes=[]
-      // for (let index = 0; index < transport.length; index++) {
-        
-        
-      //   // console.log(transport[index].routes.length)
-      //   for(let i=0;i<transport[index].routes.length;i++)
-      //   {
-      //     if (transport[index].routes[i].destination_from==from && transport[index].routes[i].destination_to==to )
-      //     {
-      //       nroutes.push(transport[index].routes[index])
-      //     }
-      //   }
 
-      // }
-      // newFares.push(nroutes)
-        
-      
-      let nroutes=[]
+      let nroutes=[] //creating local array to insert the routes information
+
+      //nested for each array to iterate routes array of each transport that has a matching route
       transport.forEach(tr=>{
         tr.routes.forEach(element => {
-          
+          //checking where in routes array is the required route
           if (element.destination_from==from && element.destination_to==to )
-          {
-            // console.log(element)
-            // routes.push(element)
+          { 
+            //inserting route in the local array of routes
             nroutes.push({'transport id':tr._id,'tranport name':tr.name,'destination_from':element.destination_from,'destination_to':element.destination_to,'fare':element.fare})
           }
         });
       })
-      newFares.push(nroutes)
+      //pushing the created local array to tranport
+      routesDetails.push(nroutes)
+
+      //old logic to store transport fares
       transport[0].routes.forEach(element => {
         if (element.destination_from==from && element.destination_to==to )
         {
-          // console.log(element)
           routes.push(element)
           fares.push(element.fare)
         }
       });
      
+      //incrementing nextDest to move to next set of destinations
       nextDest++
     }
   }
 
-  newFares.forEach(element=>{
+  //sorting the created routes array by the fare to min->maxs
+  routesDetails.forEach(element=>{
     element.sort((a,b)=> a.fare-b.fare)
   })
 
-  newFares.forEach(element=>{
-
-    // console.log('length',element.length)
-    // console.log(element[element.length])
-    minTransport+=element[0].fare
-    // console.log(minTransport)
-
-    maxTransport+=element[element.length-1].fare
+  //adding fares to minTransportFareFare and maxTransportFare
+  routesDetails.forEach(element=>{
+    minTransportFare+=element[0].fare
+    maxTransportFare+=element[element.length-1].fare
   })
-  console.log('minTrans',minTransport)
-  console.log('maxTrans',maxTransport)
-  
+
+  //interating destination array sent from client to find hotels in each destination
   for (let index = 0; index < destinations.length; index++) {
+    //aggreagation framwork used to find maximum and minimum hotel rent from each destination
     const aggregate =  [
       { $match : { "destination": new mongoose.Types.ObjectId(destinations[index])} } ,
       {
-        
-        $group:
-        {
-          _id: "$destination",
-          HotelMin: { $min: "$budget_rent" },
-          HotelMax:{ $max: "$luxury_rent" }
-        }
-      },
-
-      // { $lookup: {from: 'tripplannerdestinations', localField: '_id', foreignField: '_id', as: 'destination'} },
-      // { $project: { "_id": 1} },
-      
+        $group: { _id: "$destination", HotelMin: { $min: "$budget_rent" },HotelMax:{ $max: "$luxury_rent" }}
+      },  
     ]
+
+    //returns an array of object containing minimum and maximum values
     const hotel = await HotelModel.aggregate(aggregate).exec()
+    // console.log(hotel)
+
+    //adding minimum and maximum hotel rent from each destination to the variables
     minHotel +=  hotel[0].HotelMin
     maxHotel +=  hotel[0].HotelMax
   }
+  //old logic to calculate total fare by iterating the fares array and adding each value
   fares.forEach(fare=> totalFare+=fare)
+  //old logic to calculate min and max estimates
   minEstimate = minHotel+totalFare
   maxEstimate = maxHotel+totalFare
 
-  let newminEstimate = minHotel+minTransport
-  let newmaxEstimate = maxHotel+maxTransport
-  res.send({minHotel,maxHotel, minTransport,maxTransport,minEstimate,maxEstimate,newmaxEstimate,newminEstimate,totalFare})
-  }
+  //calculating the new min and max estimate
+  let newMinEstimate = minHotel+minTransportFare
+  let newMaxEstimate = maxHotel+maxTransportFare
+  res.send({minHotel,maxHotel, minTransportFare,maxTransportFare,minEstimate,maxEstimate,newMaxEstimate,newMinEstimate,totalFare})
+}
 module.exports.getTripPlanEstimate  = getTripPlanEstimate
