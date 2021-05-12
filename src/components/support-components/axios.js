e95cfc972907e2684c726fcfe0c028c9a71664ab
Shah Faisal <75284252+shahfaisallive@users.noreply.github.com>
2021-05-13 02:27:11 +0500

    path fixes

--- src/components/support-components/axios.js
+++ src/components/support-components/axios.js
@@ -1,12 +1,11 @@
 import axios from 'axios';
-import { useDispatch, useSelector } from 'react-redux';
 const instance = axios.create({
-    baseURL: 'http://travelogic.herokuapp.com/api',
+    baseURL: 'https://travelogic.herokuapp.com/api',
     withCredentials: true,
     credentials: 'include'
 });
 
 // instance.defaults.headers.delete['Authorization']= 
 
-export const imagePath = 'http://travelogic.herokuapp.com/uploads/images'
+export const imagePath = 'https://travelogic.herokuapp.com/uploads/images'
 export default instance;
