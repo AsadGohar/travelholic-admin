8d17bf815543a53eedbd013996a821ef5ad9498e
Shah Faisal <75284252+shahfaisallive@users.noreply.github.com>
2021-05-18 00:58:00 +0500

    .

--- src/components/support-components/axios.js
+++ src/components/support-components/axios.js
@@ -1,11 +1,14 @@
 import axios from 'axios';
 const instance = axios.create({
-    baseURL: 'https://travelogic.herokuapp.com/api',
+    // baseURL: 'https://travelogic.herokuapp.com/api',
+    baseURL: 'https://omniscient-silent-eel.glitch.me/api',
     withCredentials: true,
     credentials: 'include'
 });
 
 // instance.defaults.headers.delete['Authorization']= 
 
-export const imagePath = 'https://travelogic.herokuapp.com/uploads/images'
+// export const imagePath = 'https://travelogic.herokuapp.com/uploads/images'
+export const imagePath = 'https://omniscient-silent-eel.glitch.me/uploads/images'
+
 export default instance;
