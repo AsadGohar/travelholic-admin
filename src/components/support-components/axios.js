3889cdd9140c8a70c2234666ee0485a413792db2
AsadGohar <53337734+AsadGohar@users.noreply.github.com>
2021-05-22 05:55:46 +0500

    updated glitch url
    updated url in axios and paths

--- src/components/support-components/axios.js
+++ src/components/support-components/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 const instance = axios.create({
     // baseURL: 'https://travelogic.herokuapp.com/api',
-    baseURL: 'https://omniscient-silent-eel.glitch.me/api',
+    baseURL: 'https://travelogic.glitch.me/api',
     withCredentials: true,
     credentials: 'include'
 });
@@ -9,7 +9,7 @@ const instance = axios.create({
 // instance.defaults.headers.delete['Authorization']= 
 
 // export const imagePath = 'https://travelogic.herokuapp.com/uploads/images'
-export const imagePath = 'https://omniscient-silent-eel.glitch.me/uploads/images'
-export const tripImagePath = 'https://omniscient-silent-eel.glitch.me/uploads/trips/'
+export const imagePath = 'https://travelogic.glitch.me/uploads/images'
+export const tripImagePath = 'https://travelogic.glitch.me/uploads/trips/'
 
 export default instance;
